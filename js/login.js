03a2e3633e7c843b112082cb1740eea80f74d414
Manuelblanco0 <manuelblanco@udesa.edu.ar>
2025-11-17 17:31:32 -0300

    login.js y html

--- js/login.js
+++ js/login.js
@@ -1,15 +1,13 @@
 let formulario = document.querySelector(".formulario")
 let mail = document.querySelector(".emails input")
 let contraseña = document.querySelector(".contraseña input")
-let repetirCon = document.querySelector(".reContraseña input")
 
 let mailError = document.querySelector(".mail-invalido")
 let contraseñaError = document.querySelector(".contraseña-invalida")
-let repetirConError = document.querySelector(".reContraseña-invalida")
 
 formulario.addEventListener("submit" , function(event) {
     event.preventDefault();
-    if (mail.value == "" || contraseña.value == ""|| (contraseña.value).length<6 || contraseña.value != repetirCon.value){
+    if (mail.value == "" || contraseña.value == ""|| (contraseña.value.length <6)){
         if (mail.value == ""){
             mailError.innerHTML = "<p>Este campo es obligatorio</p>"
             mailError.style.display = "block"
@@ -22,21 +20,14 @@ formulario.addEventListener("submit" , function(event) {
             contraseñaError.innerHTML = "<p>La contraseña debe contener al menos 6 caracteres</p>"
             contraseñaError.style.display = "block"
         }
-        if (repetirCon.value != contraseña.value){
-            repetirConError.innerHTML = "<p>La contraseña no es la misma</p>"
-            repetirConError.style.display = "block"
-        }
         if(mail.value != ""){
             mailError.style.display = "none"
         }
         if(contraseña.value != ""){
             contraseñaError.style.display = "none"
         }
-        if((contraseña.value).length >= 6 ){
+        if(contraseña.value.length >= 6 ){
             contraseñaError.style.display = "none"
-        }
-        if(repetirCon.value == contraseña.value){
-            repetirConError.style.display = "none"
         }
     }
     else{
